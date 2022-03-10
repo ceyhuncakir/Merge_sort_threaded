de3499d64c8d9813adc9b9d99f29c9861358ea20
ceyhuncakir <ceyhuncakir@live.nl>
2022-03-10 17:29:44 +0100

    update new
    update new

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#include <thread>
 #include <numeric>
 #include <cmath>
+#include <thread>
 
 using namespace std::chrono;
 
-template <typename T>
-std::vector<T> merge_sort(std::vector<T> array_01, std::vector<T> array_02) {
+std::vector<int> merge_sort(std::vector<int> array_01, std::vector<int> array_02) {
 
   if(array_01.size() == 0 && array_02.size() == 0) {
     return {};
@@ -19,12 +18,12 @@ std::vector<T> merge_sort(std::vector<T> array_01, std::vector<T> array_02) {
       // head van array_02
       int array_02_head = array_02[0];
       // tail van array_02
-      std::vector<T> array_02_tail = {array_02.begin() + 1, array_02.end()};
+      std::vector<int> array_02_tail = {array_02.begin() + 1, array_02.end()};
       // sorted lijst van array_01 en array_02_tail
-      std::vector<T> sorted = merge_sort(array_01, array_02_tail);
+      std::vector<int> sorted = merge_sort(array_01, array_02_tail);
 
       // output is array_02_head + sorted
-      std::vector<T> output = {};
+      std::vector<int> output = {};
 
       output.emplace_back(array_02_head);
 
@@ -38,8 +37,8 @@ std::vector<T> merge_sort(std::vector<T> array_01, std::vector<T> array_02) {
       int array_01_head = array_01[0];
       std::vector<int> array_01_tail = {array_01.begin() + 1, array_01.end()};
 
-      std::vector<T> sorted = merge_sort(array_01_tail, array_02);
-      std::vector<T> output = {};
+      std::vector<int> sorted = merge_sort(array_01_tail, array_02);
+      std::vector<int> output = {};
 
       output.emplace_back(array_01_head);
 
@@ -52,23 +51,23 @@ std::vector<T> merge_sort(std::vector<T> array_01, std::vector<T> array_02) {
       // head van array_01
       int array_01_head = array_01[0];
       // tail van array_01
-      std::vector<T> array_01_tail = {array_01.begin() + 1, array_01.end()};
+      std::vector<int> array_01_tail = {array_01.begin() + 1, array_01.end()};
       // head van array_02
       int array_02_head = array_02[0];
       // tail van array_02
-      std::vector<T> array_02_tail = {array_02.begin() + 1, array_02.end()};
+      std::vector<int> array_02_tail = {array_02.begin() + 1, array_02.end()};
       // sorted lijst van array_01 en array_02_tail
 
       if(array_01_head < array_02_head) {
-        std::vector<T> output_01 = {};
-        std::vector<T> sorted_01 = merge_sort(array_01_tail, array_02);
+        std::vector<int> output_01 = {};
+        std::vector<int> sorted_01 = merge_sort(array_01_tail, array_02);
         output_01.emplace_back(array_01_head);
         output_01.insert(output_01.end(), sorted_01.begin(), sorted_01.end());
 
         return output_01;
       } else {
-        std::vector<T> output_02 = {};
-        std::vector<T> sorted_02 = merge_sort(array_01, array_02_tail);
+        std::vector<int> output_02 = {};
+        std::vector<int> sorted_02 = merge_sort(array_01, array_02_tail);
         output_02.emplace_back(array_02_head);
         output_02.insert(output_02.end(), sorted_02.begin(), sorted_02.end());
 
@@ -78,24 +77,16 @@ std::vector<T> merge_sort(std::vector<T> array_01, std::vector<T> array_02) {
   }
 }
 
-template <typename T>
-std::vector<T> recursive_merge_sort(std::vector<T> data) {
+std::vector<int> recursive_merge_sort(std::vector<int> data) {
   if(data.size() == 1) {
     return data;
   } else {
 
     std::size_t const middle = data.size() / 2;
-    std::vector<T> first(data.begin(), data.begin() + middle);
-    std::vector<T> second(data.begin() + middle, data.end());
+    std::vector<int> first(data.begin(), data.begin() + middle);
+    std::vector<int> second(data.begin() + middle, data.end());
 
-    std::vector<T> first_output = {};
-    std::vector<T> second_output = {};
-
-
-    first_output = recursive_merge_sort(first);
-    second_output = recursive_merge_sort(second);
-
-    return merge_sort(first_output, second_output);
+    return merge_sort(recursive_merge_sort(first), recursive_merge_sort(second));
   }
 }
 
@@ -110,21 +101,31 @@ std::vector<std::vector<int>> get_splits(std::vector<int> data, int threads) {
   return split_data;
 }
 
-template <typename T>
-std::vector<T> threaded_merge_sort(std::vector<T> data, int max_threads) {
-  std::vector<std::vector<int>> splits = get_splits(data, max_threads);
-  std::vector<T> value = {};
+std::vector<int> threaded_merge_sort(std::vector<int> data, int max_threads) {
+  std::vector<int> output = {};
 
-  for(int i = 0; i < max_threads; i++) {
-    std::cout << splits[i];
-    std::thread thread_1(recursive_merge_sort, splits[i]);
+  if(max_threads > 1) {
+    std::vector<std::vector<int>> splits = get_splits(data, max_threads);
+    std::vector<std::thread> threads;
+
+    for(int i = 0; i < splits.size(); i++) {
+      threads.emplace_back(std::thread(recursive_merge_sort, std::ref(splits[i])));
+    }
+    for(auto& t1 : threads) {
+      t1.join();
+    }
+  } else {
+    return recursive_merge_sort(data);
   }
-
-  return value;
+  // we returnen de output van de threads
+  return output;
 }
 
+// testen of
+
 int main() {
-  std::vector<int> data = {14, 5, 9, 10, 23, 3, 26, 1};
+  std::vector<int> data = {10000};
+  std::generate(data.begin(), data.end(), std::rand());
   std::vector<int> sorted_array;
 
   auto start = high_resolution_clock::now();
